fb20105c4680a9c6e00829d9b0e3299fb7eb809f
dhiraj <22m0771@iitb.ac.in>
2022-11-22 14:56:32 +0530

    pyplot options added along with style option

--- dataviz/dataviz/graphs.py
+++ dataviz/dataviz/graphs.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib import style
 from base64 import b64decode
 from base64 import b64encode
 from io import BytesIO
@@ -12,22 +13,22 @@ def plot_buffer_to_utf8(plot_buffer):
     plot_utf8 = plot_base64.decode('utf-8')
 
     return plot_utf8
-
-def get_line_graph(data, x_column, y_column):
+# Line Graph plot
+def get_line_graph(data, x_column, y_column,style):
+    plt.style.use(style)
     if (x_column == " "):
-        plt.plot(data[y_column])
+        plt.plot(data[y_column[0]])
     else:
         x_column_data = data[x_column]
         y_column_data = data[y_column]
         zippedXY = list(zip(x_column_data,y_column_data))
         zippedXY.sort(key=lambda x: x[0])
-        
         x_column_data = [x for (x,y) in zippedXY]
         y_column_data = [y for (x,y) in zippedXY]
-
-        plt.plot(x_column_data,y_column_data)
+        plt.plot(x_column_data,y_column_data, label=y_column,mew=2, linewidth=2)
         plt.xlabel(x_column)
     plt.grid(b='on')
+    plt.xticks(rotation=90)
     plt.ylabel(y_column)
     plot_buffer = BytesIO()
     plt.savefig(plot_buffer, format="png")
@@ -36,8 +37,9 @@ def get_line_graph(data, x_column, y_column):
     encoded_plot = plot_buffer_to_utf8(plot_buffer)
     return encoded_plot
 
-
-def get_bar_graph(data, x_column, y_column):
+# Bar graph
+def get_bar_graph(data, x_column, y_column,style):
+    plt.style.use(style)
     if (x_column == " "):
         return None
     else:
@@ -51,8 +53,9 @@ def get_bar_graph(data, x_column, y_column):
 
     encoded_plot = plot_buffer_to_utf8(plot_buffer)
     return encoded_plot
-
-def get_scatter_plot(data, x_column, y_column):
+# Scatter Plot
+def get_scatter_plot(data, x_column, y_column,style):
+    plt.style.use(style)
     if (x_column == " "):
         return None
     else:
@@ -66,14 +69,68 @@ def get_scatter_plot(data, x_column, y_column):
 
     encoded_plot = plot_buffer_to_utf8(plot_buffer)
     return encoded_plot
-
-def get_hexbin_plot(data,x_column,y_column):
+# Hexbin Plot
+def get_hexbin_plot(data,x_column,y_column,style):
+    plt.style.use(style)
     if (x_column == " "):
         return None
     else:
-        plt.hexbin(data[x_column], data[y_column])
+        plt.hexbin(data[x_column], data[y_column],gridsize=20)
         plt.xlabel(x_column)
     plt.grid(b='on')
+    plt.ylabel(y_column)
+    plot_buffer = BytesIO()
+    plt.savefig(plot_buffer, format="png")
+    plt.close()
+
+    encoded_plot = plot_buffer_to_utf8(plot_buffer)
+    return encoded_plot
+
+#Stem Plot functionality
+
+def get_stem_plot(data, x_column, y_column,style):
+    plt.style.use(style)
+    if (x_column == " "):
+        return None
+    else:
+        plt.stem(data[x_column], data[y_column])
+        plt.xlabel(x_column)
+    plt.grid(b='on')
+    plt.ylabel(y_column)
+    plot_buffer = BytesIO()
+    plt.savefig(plot_buffer, format="png")
+    plt.close()
+
+    encoded_plot = plot_buffer_to_utf8(plot_buffer)
+    return encoded_plot
+
+# Histogram 2D plot
+
+def get_hist2d_plot(data, x_column, y_column,style):
+    plt.style.use(style)
+    if (x_column == " "):
+        return None
+    else:
+        plt.hist2d(data[x_column], data[y_column])
+        plt.xlabel(x_column)
+    # plt.grid(b='on')
+    plt.ylabel(y_column)
+    plot_buffer = BytesIO()
+    plt.savefig(plot_buffer, format="png")
+    plt.close()
+
+    encoded_plot = plot_buffer_to_utf8(plot_buffer)
+    return encoded_plot
+
+# Triplot 
+def get_triplot_plot(data, x_column, y_column,style):
+    plt.style.use(style)
+    if (x_column == " "):
+        return None
+    else:
+        plt.triplot(data[x_column], data[y_column])
+        plt.xlabel(x_column)
+    # plt.grid(b='on')
     plt.ylabel(y_column)
     plot_buffer = BytesIO()
     plt.savefig(plot_buffer, format="png")
--- dataviz/dataviz/views.py
+++ dataviz/dataviz/views.py
@@ -14,7 +14,7 @@ import datetime
 import os
 import glob
 
-TIMEOUT_TIME = 20
+TIMEOUT_TIME = 300
 
 uuid_file_dict = {}
 uuid_file_last_modified_datetime_dict = {}
@@ -26,23 +26,36 @@ sample_files_path = "./static/sample_datasets"
 
 #Loading Sample Datasets
 files = glob.glob(sample_files_path + "/*.csv")
+#print(files)
 for file in files:
     with open(file,"rb") as csvfile_object:
         csvfile = BytesIO(csvfile_object.read())
-        filename = file.split('\\')[-1].split('.')[0]
+        filename = file.split('/')[-1].split('.')[0]
         sample_file__list.append(filename)
         sample_files_image_path_dict[filename] = "sample_datasets_images/"+filename+".jpg"
         uuid_file_dict[filename] = csvfile
         uuid_file_last_modified_datetime_dict[filename] = datetime.datetime.now()
 
+#print(sample_file__list)
 
-
-graph_list = ["Line Graph", "Bar Graph","Scatter Plot","HeXBin Plot"]
+graph_list = ["Line Graph", "Bar Graph","Scatter Plot","HeXBin Plot","Stem Plot", "2D Histogram", "TriPlot"]
+# Which matplotlib style sheet to use
+style_list = ['Solarize_Light2', '_classic_test_patch', '_mpl-gallery', 
+            '_mpl-gallery-nogrid', 'bmh', 'classic', 'dark_background', 
+            'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'seaborn-v0_8', 
+            'seaborn-v0_8-bright', 'seaborn-v0_8-colorblind', 'seaborn-v0_8-dark', 
+            'seaborn-v0_8-dark-palette', 'seaborn-v0_8-darkgrid', 'seaborn-v0_8-deep', 
+            'seaborn-v0_8-muted', 'seaborn-v0_8-notebook', 'seaborn-v0_8-paper', 
+            'seaborn-v0_8-pastel', 'seaborn-v0_8-poster', 'seaborn-v0_8-talk', 
+            'seaborn-v0_8-ticks', 'seaborn-v0_8-white', 'seaborn-v0_8-whitegrid']
 graph_function_dict = {
     "Line Graph": graphs.get_line_graph, 
     "Bar Graph": graphs.get_bar_graph,
     "Scatter Plot" : graphs.get_scatter_plot,
-    "HeXBin Plot" : graphs.get_hexbin_plot}
+    "HeXBin Plot" : graphs.get_hexbin_plot,
+    "Stem Plot" : graphs.get_stem_plot,
+    "2D Histogram" : graphs.get_hist2d_plot,
+    "TriPlot" : graphs.get_triplot_plot}
 
 
 def delete_unused_files_decorator(func):
@@ -96,12 +109,15 @@ def show_data(request, csv_uuid):
         param = {"data_columns": list(data.columns), "data_values": list(
             data.head().values), "graph_list": graph_list}
         param["plot"] = None
+        param["plot_style_list"] = style_list
         if (request.method == 'GET'):
             graph_type = request.GET.get("graph")
             y_column = request.GET.get("ycolumn")
             x_column = request.GET.get("xcolumn")
+            # getting style param
+            style = request.GET.get("style")
             if (graph_type is not None):
-                plot = get_graph(data, graph_type, x_column, y_column)
+                plot = get_graph(data, graph_type, x_column, y_column, style)
                 param["plot"] = plot
         return render(request, 'show_data.html', param)
     else:
@@ -119,11 +135,11 @@ def load_data(uuid_key):
 
     return data
 
-def get_graph(data, graph_type, x_column, y_column):
+def get_graph(data, graph_type, x_column, y_column, style):
 
     plot = None
     if graph_type in graph_function_dict:
         plot = graph_function_dict[graph_type](
-            data=data, x_column=x_column, y_column=y_column)
+            data=data, x_column=x_column, y_column=y_column, style=style)
     
     return plot
